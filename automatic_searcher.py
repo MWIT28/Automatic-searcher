622877782a3f9c529e1a47abb67f5268e52cd5da
Marcin Witkowski <marcin94witkowski@gmail.com>
2022-08-23 12:15:04 +0200

    Add files via upload

---
+++ automatic_searcher.py
@@ -0,0 +1,16 @@
+import webbrowser
+import time
+filepath = "test2.txt"
+f = open(filepath, "r", encoding="utf-8")
+i = 0
+while i < 7:
+
+    webbrowser.get('C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe %s').open_new_tab('https://www.google.pl/search?q=' + f.readline())
+    i = i + 1
+    time.sleep(2)
+
+
+f.close()
+
+
+
